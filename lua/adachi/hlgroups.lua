414ae764837350f0d26acb417e16423cdfc8ef50
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-04-01 13:35:24 +0200

    add(lua/hlgroups): updated terminal colors

--- lua/adachi/hlgroups.lua
+++ lua/adachi/hlgroups.lua
@@ -2,22 +2,22 @@ local M = {}
 
 ---@param theme AdachiTheme
 local function setup_terminal_colors(theme)
-  vim.g.terminal_color_0  = theme.bg0[1]
-  vim.g.terminal_color_8  = theme.bg1[1]
-  vim.g.terminal_color_1  = theme.gb.neutral['red'][1]
-  vim.g.terminal_color_9  = theme.red[1]
-  vim.g.terminal_color_2  = theme.gb.neutral['green'][1]
-  vim.g.terminal_color_10 = theme.green[1]
-  vim.g.terminal_color_3  = theme.gb.neutral['red'][1]
-  vim.g.terminal_color_11 = theme.red[1]
-  vim.g.terminal_color_4  = theme.gb.neutral['blue'][1]
-  vim.g.terminal_color_12 = theme.blue[1]
-  vim.g.terminal_color_5  = theme.gb.neutral['purple'][1]
-  vim.g.terminal_color_13 = theme.purple[1]
-  vim.g.terminal_color_6  = theme.gb.neutral['aqua'][1]
-  vim.g.terminal_color_14 = theme.aqua[1]
-  vim.g.terminal_color_7  = theme.fg4[1]
-  vim.g.terminal_color_15 = theme.fg1[1]
+  vim.g.terminal_color_0  = theme.bg1[1]
+  vim.g.terminal_color_8  = theme.bg0[1]
+  vim.g.terminal_color_1  = theme.red[1]
+  vim.g.terminal_color_9  = theme.gb.neutral['red'][1]
+  vim.g.terminal_color_2 = theme.green[1]
+  vim.g.terminal_color_10  = theme.gb.neutral['green'][1]
+  vim.g.terminal_color_3 = theme.yellow[1]
+  vim.g.terminal_color_11  = theme.gb.neutral['yellow'][1]
+  vim.g.terminal_color_4 = theme.blue[1]
+  vim.g.terminal_color_12  = theme.gb.neutral['blue'][1]
+  vim.g.terminal_color_5 = theme.purple[1]
+  vim.g.terminal_color_13  = theme.gb.neutral['purple'][1]
+  vim.g.terminal_color_6 = theme.aqua[1]
+  vim.g.terminal_color_14  = theme.gb.neutral['aqua'][1]
+  vim.g.terminal_color_7 = theme.bg3[1]
+  vim.g.terminal_color_15  = theme.bg4[1]
 end
 
 ---@class HLGroup
